9ec890a765fb1b2cc04d725f26c89609ed03c532
warunacds <warunacds@gmail.com>
2015-11-01 13:01:09 +0530

    Initial Commit

---
+++ WDAddressBookSync/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WDAddressBookSync
+//
+//  Created by Waruna de Silva on 11/1/15.
+//  Copyright © 2015 Waruna de Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WDAddressBookSync/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WDAddressBookSync
+//
+//  Created by Waruna de Silva on 11/1/15.
+//  Copyright © 2015 Waruna de Silva. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
